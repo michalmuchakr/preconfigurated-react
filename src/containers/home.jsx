9880ae6ca941b7c2780e72f3fbabad0fcd2e7f46
Michal Mucha <michal.mucha.kr@gmail.com>
2020-07-12 00:17:17 +0200

    Update dependencies

--- src/containers/home.jsx
+++ src/containers/home.jsx
@@ -1,9 +1,31 @@
 import React from "react";
 
+// Function to download data to a file
+function download(data, filename, type) {
+  var file = new Blob([data], { type: type });
+  if (window.navigator.msSaveOrOpenBlob)
+    // IE10+
+    window.navigator.msSaveOrOpenBlob(file, filename);
+  else {
+    // Others
+    var a = document.createElement("a"),
+      url = URL.createObjectURL(file);
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(function () {
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    }, 0);
+  }
+}
+
 function Home() {
   return (
     <div className="home-page page">
       <h1>Home</h1>
+      <button onClick={() => download("doe", "some-name", "json")}>save</button>
     </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,53 @@
-const HtmlWebPlugin = require('html-webpack-plugin'),
-  ExtractTextPlugin = require("extract-text-webpack-plugin"),
-  path = require('path');
+const HtmlWebPlugin = require("html-webpack-plugin"),
+  path = require("path");
 
 const htmlPlugin = new HtmlWebPlugin({
   template: "./src/index.html",
-  filename: "./index.html"
+  filename: "./index.html",
 });
 
-const extractPlugin = new ExtractTextPlugin('style.css');
-
 module.exports = {
-  entry: './src/index.jsx',
+  entry: "./src/index.jsx",
+  output: {
+    filename: "main.js",
+    path: path.resolve("dist"),
+    publicPath: "/",
+  },
   resolve: {
-    modules: [path.resolve(__dirname, './src'), 'node_modules'],
+    modules: [path.resolve(__dirname, "./src"), "node_modules"],
     alias: {
-      styles: path.resolve(__dirname, './src/styles')
-    }
+      styles: path.resolve(__dirname, "./src/styles"),
+    },
+  },
+  node: {
+    fs: "empty",
   },
   stats: "errors-only",
   module: {
-    rules: [{
-      test: /\.js$|\.jsx$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader"
-      }
-    }, {
-      test: /\.scss$/,
-      use: [
-        // Creates `style` nodes from JS strings
-        'style-loader',
-        // Translates CSS into CommonJS
-        'css-loader',
-        // Compiles Sass to CSS
-        'sass-loader',
-      ],
-
-    }]
+    rules: [
+      {
+        test: /\.js$|\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
   },
   devServer: {
     historyApiFallback: true,
-    port: 1234
+    port: 1234,
   },
-  plugins: [
-    htmlPlugin,
-    extractPlugin
-  ]
-}
+  plugins: [htmlPlugin],
+};
